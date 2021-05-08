2159b4d770bdd660773c6664be8ce8468d535940
Grauer W01f <grauerwf@gmail.com>
2021-05-08 17:15:11 +0300

    Minimal working

--- kubetpl/kubetpl.py
+++ kubetpl/kubetpl.py
@@ -9,6 +9,7 @@ from jinja2 import exceptions
 
 
 required_resources_parameters = ['name', 'path', 'include']
+kubectl_cmd_tpl = "cat <<EOF | {1} {2} --context {3} -f - \n{0}\nEOF\n"
 
 
 def get_resource_list(resource_list):
@@ -55,7 +56,7 @@ def parse_args():
                         default='kubectl')
 
     parser.add_argument('command',
-                        help='Template resourceset'
+                        help='Template resource set'
                              ' and pass to "kubectl <command>"')
     parser.add_argument('file',
                         help='Resource Set to templating')
@@ -74,29 +75,28 @@ def find_resource_location(resource_path):
     exit(1)
 
 
-def template_resources(resources_list, values):
-    templated_resources = ''
-    resources_to_template = []
+def template_resources(resources_list, context, values):
     for resource in resources_list:
-        resource_location = find_resource_location(resource)
-        if os.path.isfile(resource_location):
-            resources_to_template.append(resource_location)
-        elif os.path.isdir(resource_location):
-            resources_to_template.extend(
-                [str(os.path.sep).join([resource_location, file])
-                 for file in os.listdir(resource_location)
-                 if file.endswith(".yml")
-                 or file.endswith(".yaml")
-                 or file.endswith(".json")])
-    for resource in resources_to_template:
         with open(resource) as template_file:
             try:
                 template = Template(template_file.read())
-                templated_resources += template.render(values) + '\n'
+                templated_resource = template.render(values)
+                if args.command == 'template':
+                    print('### File: {0}'.format(resource))
+                    print(templated_resource)
+                else:
+                    kubectl_cmd = kubectl_cmd_tpl.format(templated_resource,
+                                                         args.kubectl_path,
+                                                         args.command,
+                                                         context)
+                    res = os.system(kubectl_cmd)
+                    if res != 0:
+                        print('kubectl error on file {0}'.format(resource))
+                        exit(1)
             except exceptions.TemplateSyntaxError as exc:
-                print("Error templating resource {0}, {1}".format(resource, exc.message))
+                print("Error templating resource {0}, {1}".format(resource,
+                                                                  exc.message))
                 exit(1)
-    return templated_resources
 
 
 args = parse_args()
@@ -104,6 +104,7 @@ args = parse_args()
 
 def main():
     resources_to_template = []
+    included_resources = []
     with open(args.file) as resource_set_file:
         resource_set = yaml.load(resource_set_file.read(),
                                  Loader=yaml.SafeLoader)
@@ -114,28 +115,31 @@ def main():
         for resource in resource_set_resources:
             for include in args.include:
                 if resource.startswith(include):
-                    resources_to_template.append(resource)
+                    included_resources.append(resource)
     elif len(args.exclude) > 0:
         for resource in resource_set_resources:
             for include in args.exclude:
                 if not resource.startswith(include):
-                    resources_to_template.append(resource)
+                    included_resources.append(resource)
 
     else:
-        resources_to_template = resource_set_resources
+        included_resources = resource_set_resources
     if len(args.vars) > 0:
         for var in args.vars:
             (var_key, var_value) = var.split('=')
             tpl_vars[var_key] = var_value
-    templated_resources = template_resources(resources_to_template, tpl_vars)
-    if args.command == 'template':
-        print(templated_resources)
-    else:
-        kubectl_command_tpl = 'echo "{0}" | {1} {2} --context {3} -f -'
-        os.system(kubectl_command_tpl.format(templated_resources,
-                                             args.kubectl_path,
-                                             args.command,
-                                             resource_set['context']))
+    for resource in included_resources:
+        resource_location = find_resource_location(resource)
+        if os.path.isfile(resource_location):
+            resources_to_template.append(resource_location)
+        elif os.path.isdir(resource_location):
+            resources_to_template.extend(
+                [str(os.path.sep).join([resource_location, file])
+                 for file in os.listdir(resource_location)
+                 if file.endswith(".yml")
+                 or file.endswith(".yaml")
+                 or file.endswith(".json")])
+    template_resources(resources_to_template, resource_set['context'], tpl_vars)
 
 
 if __name__ == '__main__':
